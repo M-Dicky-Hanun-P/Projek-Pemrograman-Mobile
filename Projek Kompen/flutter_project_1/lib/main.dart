d24ab1947fd481a61bf4f738f65fd2d19cef4084
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2024-11-11 12:34:54 +0700

    Progress 2.2

--- Projek Kompen/flutter_project_1/lib/main.dart
+++ Projek Kompen/flutter_project_1/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Override metode build() untuk mendeskripsikan tampilan widget.
+    // Override metode build() untuk mendeskripsikan tampilan widget bagian ini.
     return MaterialApp(
       // Mengembalikan widget MaterialApp, yang merupakan widget dasar untuk aplikasi Material.
       debugShowCheckedModeBanner:
